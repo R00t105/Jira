48bad7635baa2099590a5d9ec2111ba435636019
R00t105 <moclaymo@gmail.com>
2025-12-30 16:44:50 +0200

    Create Entities

---
+++ src/main/java/jira/Entity/AppUser.java
@@ -0,0 +1,24 @@
+package jira.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+}
